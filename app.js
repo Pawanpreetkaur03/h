83a02e555350555161e6c8cd51ac0c116bfbf67c
pawanpreetkaur03 <pawanpreetkaur529@gmail.com>
2025-06-09 13:55:12 -0600

    update

--- app.js
+++ app.js
@@ -13,46 +13,52 @@ const ollama = new Ollama({
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-
-//endpoint for the chat stream
-app.post('/api/chat-stream', async (req, res) =>{
+app.post('/api/vhat-stream', async (req, res) => {
+  
   try {
+    
 
-    //prompt from the user
-    const { prompt } = req.body;
+       //prompt from the user
+       const { prompt } = req.body;
+       
+       //initiate the stream from the model
+       //throws prompt from user into the model
+       const stream = await ollama.chat({
+              model: 'deepseek-r1:7b' ,
+              message: [{role: 'user' , content: prompt}],
+              stream: true
+       });
+   
+      res.header('Content-type', 'text/plain');
+      for await(const chunk of stream){
+        try {
+          //check content in each chunk using null aware operator "?"
+              if(chunk.message?.content){
+               //write the actual chunks with data to the stream
+                res.write(chunk.message.content);
 
-    //initiate the stream from the model
-    //throws stream from user into the model
-    const strem = await ollama.chat({
-      model: 'deepseek-r1:7b',
-      messages: [{role: 'user', content: prompt}],
-      stream: true
-    });
 
-    res.Header('Content-type', 'text/plain');
+              }
+        } catch (streamError) {
+          console.error(Error with stream: ${streamError});
+          break;
+          
+        }
+      }
 
-    for await (const chunk of stream){
-      try {
-        
-        //check content in each chunk using null aware operator "?."
-          if(chunk.messsage?.content){
-            
-        //write the actual chunks with data to stream
-          res.write(chunk.message.content);
-          }
-  } catch (streamError) {
-       console.error(`Error with strem: ${streamError}`);
-  }
-}
-
+res.end();
+      
   } catch (error) {
-      res.status(500).send('Internal server Error');
+    
+    res.status(500).send(Internal Server Error: ${error});
   }
+
 });
 
 
 
+
 app.listen(PORT, () => {
-  console.log(`Connected to Port ${PORT}`);
+  console.log(Connected to Port ${PORT});
 
 });
--- public/message.js
+++ public/message.js
@@ -1,42 +1,86 @@
 //declaring global variables
+
 let abortController = null;
 let isGenerating = false;
 
 //asynchronous function that is invoked when "send" button is pushed
-async function sendMessage(message) {
 
+async function sendMessage(){
+    
+    //grabbing elements of the chat window
 
-//grabbing elements of the chat window
-    const inputBox = document.getElementById("inputBox");
-    const chatBox = document.getElementById("chatBox");
-    const stopbtn = document.getElementById("stop-btn");
-     //trim rempves for prompt, if none we jump out of function
-    const prompt = inputBox.value.trim();
-// checking for prompt
+    const inputBox = document.getElementById("input-box");
+    const chatBox = document.getElementById("chat-box");
+    const stopBtn = document.getElementById("stop-btn");
+    
+    //trim rempves for prompt, if none we jump out of function
+
+    const prompt = inputBox.Value.trim();
+    
+    // checking for prompt
+
     if(!prompt) return;
-    if (isGenerating) {
-       stopGeneration(); 
-        
+
+    if(isGenerating){
+        stopGenerating();
     }
+         
+     chatBox.innerHTML += <div class="user-message">You: ${prompt}</div>;
+    
+       try {
+           // create a new instance of the abortcontroller, which stops the function process
+           abortController = new AbortController();
+           //because the model is generating. the prompt has been sent to the back end.
+           isGenerating = true;
 
-    chatBox.innerHTML += <div class="user-message">You: ${prompt}</div>;
+           //network request to the api endpoint
+           const response = await fetch('http://localhost:3000/api/chat-stream', {
+               method: 'POST',
+               header: {'Content-Type': 'application/json'},
+               body: JSON.stringify({prompt}),
+               signal: abortController.signal
+        });
+            //read the response with built-in method
+            const reader = response.body.getReader();
 
-    try {
-         // create a new instance of the abortcontroller, which stops the function process
-        
+            //create a new instance of text decoder
+            const decoder = new TextDecoder();
+
+            //create the bot-message div element
+            const botDiv = document.createElement('div');
+            botDiv.className = 'bot-messaage';
+            botDiv.textContent = 'Ai:' ;
+            chatBox.appendChild(botDiv);
+
+            //iterate the process of streaming  the data to the front in batches
+            while (true) {
+                const {done, value } = await reader.read();
+                if (done) break;
+
+                //use additional assignment operator to decode bot message and send to botdiv
+                botDiv.textContent += decoder.decode(value, {stream: true});
+
+                //scroll box as data is streaming out 
+                chatBox.scrollTop = chatBox.scrollHeight;
+
+                
+            }
+
     }catch (error) {
-
-
-
+       chatBox.innerHTML = <div class="bot-message" style="color:red;">${error.message}</div>;
+    } finally {
+    stopBtn.disable = true;
+    isGenerating = false;
     }
 
-}
-
-function stopGeneration() {
-    if (abortController) {
+}   
+ 
+//
+ function stopGenerating (){
+    if(abortController){
         abortController.abort();
-        
     }
     isGenerating = false;
-    document.getElementById('stop-btn').disable = true;
-}
+    document.getElementById('stop-btn').disable = true;
+
+ }
